2f39cbd16ea101a968a2951c2490b02d2272b9e1
Arthur Fonseca <arthur.elf95@gmail.com>
2024-02-09 17:07:59 -0300

    FEAT: Aula 4

---
+++ Básico/Aula 4/index.js
@@ -0,0 +1,11 @@
+const cpf = '12345678912';
+let nome = 'Arthur Emilio Lopes Fonseca';
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const multiplicacao = primeiroNumero * segundoNumero;
+
+console.log(nome);
+console.log (primeiroNumero, 'X' ,segundoNumero, ':' ,multiplicacao);
+console.log('5 X 10 :' ,5*10);
+
