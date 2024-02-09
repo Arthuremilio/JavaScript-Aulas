63dc49a3c0b063a01109991b218ddab39590dbf7
Arthur Fonseca <arthur.elf95@gmail.com>
2024-02-09 16:34:08 -0300

    FEAT: Aula 3

---
+++ Básico/Aula 3/index.js
@@ -0,0 +1,7 @@
+let nome = 'Arthur'; 
+
+console.log(nome, 'nasceu em 1995');
+console.log(nome, 'conheceu Amanda em 2015')
+console.log(nome, 'casou-se com Amanda em 2020');
+console.log('Amanda teve um filho com' ,nome, 'em 2020');
+console.log('O filho de' ,nome, 'se chama Augusto');
